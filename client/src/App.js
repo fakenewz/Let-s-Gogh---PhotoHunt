a50186061c49256a98affecfada6feb3dd023da8
Whitney Shirk <whitney.shirk@gmail.com>
2018-07-13 20:48:28 -0500

    whit fix working locally

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-// import Books from "./pages/Books";
-import Question from "./pages/Question";
 import Books from "./pages/Books";
 import Admindash from "./pages/Admindash";
 import Adminquiz from "./pages/Adminquiz";
@@ -11,7 +9,7 @@ import Nav from "./components/Nav";
 
 const App = () => (
   <Router>
-   <div>
+    <div>
       <Nav />
       <Switch>
         <Route exact path="/" component={Books} />
@@ -21,15 +19,6 @@ const App = () => (
         <Route exact path="/books/:id" component={Detail} />
         <Route component={NoMatch} />
       </Switch>
-    </div> 
-    <div>
-      <Nav />
-    <Switch> 
-        <Route exact path="/" component={Question} />
-        <Route exact path="/question" component={Question} />
-        <Route exact path="/question/:id" component={Detail} />
-        <Route component={NoMatch} />
-     </Switch>
     </div>
   </Router>
 );
