d8c113d29b20f24979ce80e427d05cc8618e6ea3
Whitney Shirk <whitney.shirk@gmail.com>
2018-07-13 19:36:23 -0500

    whit added new adminquiz page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Books from "./pages/Books";
 import Admindash from "./pages/Admindash";
+import Adminquiz from "./pages/Adminquiz";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
@@ -14,6 +15,7 @@ const App = () => (
         <Route exact path="/" component={Books} />
         <Route exact path="/books" component={Books} />
         <Route exact path="/admindash" component={Books}/>
+        <Route exact path="/adminquiz" component={Books}/>
         <Route exact path="/books/:id" component={Detail} />
         <Route component={NoMatch} />
       </Switch>
---
+++ client/src/pages/Adminquiz/index.js
@@ -0,0 +1,1 @@
+export { default } from "./Adminquiz.js";
