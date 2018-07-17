0523355ef2eba07ad90a0b15a633f415518264ef
popstar123 <wwu3@saic.edu>
2018-07-16 22:24:19 -0500

    W Added

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Nav from "./components/Nav";
 import Books from "./pages/Books";
 import Admindash from "./pages/Admindash";
 import Adminquiz from "./pages/Adminquiz";
-import Detail from "./pages/Detail";
-import NoMatch from "./pages/NoMatch";
-import Nav from "./components/Nav";
 import Sign_In from "./pages/Sign_In";
 import Quiz_Creation from "./pages/Quiz_Creation";
 import Register from "./pages/Register";
 import Logout from "./pages/Logout";
+import NoMatch from "./pages/NoMatch";
 
  
 const App = () => (
@@ -17,15 +16,13 @@ const App = () => (
     <div>
       <Nav />
       <Switch>
+        <Route exact path="/" component={Books} />
         <Route exact path="/login" component={Sign_In} />
         <Route exact path="/register" component={Register} />
         <Route exact path="/quizcreation" component={Quiz_Creation} />
         <Route exact path="/logout" component={Logout} />
-        <Route exact path="/" component={Books} />
-        <Route exact path="/books" component={Books} />
-        <Route exact path="/admindash" component={Books}/>
+        <Route exact path="/admindash" component={Admindash}/>
         <Route exact path="/adminquiz" component={Adminquiz}/>
-        <Route exact path="/books/:id" component={Detail} />
         <Route component={NoMatch} />
       </Switch>
     </div>
