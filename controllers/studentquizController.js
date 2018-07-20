275833fc94f5dbe4eee5e467459c458d6b9d97f9
Whitney Shirk <whitney.shirk@gmail.com>
2018-07-19 20:15:52 -0500

    whit add studentquizcontroller

---
+++ controllers/studentquizController.js
@@ -0,0 +1,16 @@
+const db = require("../models");
+
+module.exports = {
+  create: function(req, res) {
+    db.Quiz
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  update: function(req, res) {
+    db.Quiz
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
+};
