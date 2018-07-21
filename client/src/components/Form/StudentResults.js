528acd740bcdec8db66e614c59e855a4f04e6867
Whitney Shirk <whitney.shirk@gmail.com>
2018-07-21 14:39:08 -0500

    whit add

---
+++ client/src/components/Form/StudentResults.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+// This will get the articles we scraped from the mongoDB
+app.get("/articles", function(req, res) {
+    // Grab every doc in the Articles array
+    Article.find({}, function(error, doc) {
+      // Log any errors
+      if (error) {
+        console.log(error);
+      }
+      // Or send the doc to the browser as a json object
+      else {
+        res.json(doc);
+      }
+    });
