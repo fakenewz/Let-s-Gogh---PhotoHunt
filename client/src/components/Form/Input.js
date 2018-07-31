b99b186ab921fbb6bcee42f9f14ae665d48ec9f5
Sean <sananda.som@gmail.com>
2018-07-30 22:24:41 -0500

    front end tweaks

--- client/src/components/Form/Input.js
+++ client/src/components/Form/Input.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export const Input = props => (
   <div className="form-group">
-    <input className="form-control" {...props} />
+    <input style={{backgroundColor:'#000000', color:'#ffce00', borderColor:'#ffce00' }} className="form-control" {...props} />
   </div>
 );
